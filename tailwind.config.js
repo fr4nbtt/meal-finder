75c70f02df99154ac614853c4fed52acdda89896
fr4nbtt <batet.francisco@gmail.com>
2024-12-17 23:08:48 -0300

    feat: Set up project HTML & Base CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-green": "#12372A",
+        "medium-green": "#436850",
+        "light-green": "#ADBC9F",
+        cream: "#FBFADA",
+        "soft-black": "#111827",
+      },
+      fontFamily: {
+        sans: ["Nunito", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
